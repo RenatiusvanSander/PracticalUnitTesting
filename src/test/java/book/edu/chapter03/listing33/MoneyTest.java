eae60ab7891d71e2153c0d495e639f449e3913d7
RenatiusvanSander / R. Meier <remad@web.de>
2021-04-03 19:41:59 +0200

    master: Practical Unit Testing chapter 03 listing 33

--- src/test/java/book/edu/chapter03/listing33/MoneyTest.java
+++ src/test/java/book/edu/chapter03/listing33/MoneyTest.java
@@ -1,5 +1,16 @@
 package book.edu.chapter03.listing33;
 
+import book.edu.chapter03.listing32.Money;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MoneyTest {
 
+  @Test
+  void constructorShouldSetAmountAndCurrency() {
+    Money money = new Money(10, "USD");
+
+    assertThat(money.getAmount()).isEqualTo(10);
+    assertThat(money.getCurrency()).isEqualTo("USD");
+  }
 }
