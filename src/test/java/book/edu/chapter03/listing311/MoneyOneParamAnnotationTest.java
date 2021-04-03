691797c875a3c50df7a1b7544dede05792def904
RenatiusvanSander / R. Meier <remad@web.de>
2021-04-03 20:38:50 +0200

    master: Practical Unit Testing chapter 03 listing 311

---
+++ src/test/java/book/edu/chapter03/listing311/MoneyOneParamAnnotationTest.java
@@ -0,0 +1,29 @@
+package book.edu.chapter03.listing311;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import book.edu.chapter03.listing32.Money;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+/**
+ * A parameeriyed JUnit test for {@link Money}
+ */
+public class MoneyOneParamAnnotationTest {
+
+  /**
+   * Constrcucts 3-times the instances of {@link Money} with 3 different amounts and checks the
+   * amount is equal as expected.
+   *
+   * @param amount the amount, for example 10
+   */
+  @ParameterizedTest
+  @ValueSource(ints = {10, 15, 50})
+  void constructorShouldSetAmountAndCurrency(int amount) {
+    Money momey = new Money(amount, "USD");
+
+    assertThat(momey.getAmount()).isEqualTo(amount);
+
+  }
+
+}
