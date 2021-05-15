39af2d24a198427412093fe27585001cc998493e
RenatiusvanSander / R. Meier <remad@web.de>
2021-05-15 21:25:05 +0200

    main: excercise chapter 4 listing 4111 start

---
+++ src/test/java/book/edu/exercises/chapter04/listing4111/PasswordValidatorTest.java
@@ -0,0 +1,25 @@
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+* Unit test for {@link PasswordValidator}
+*/
+public class PasswordValidatorTest {
+
+  /**
+  * Sets up 
+  */
+  @BeforeEach
+  void setUp() {
+  }
+  
+  /**
+  * Tests 
+  */
+  @Test
+  void shouldSetVary() {
+  }
+}
